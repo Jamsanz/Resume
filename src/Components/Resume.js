9f62e610b553da38870129e04f47689205f7fbae
Muhammad Sani <jamiludeensani@gmail.com>
2022-01-26 06:52:03 +0100

    work experience md

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -23,7 +23,20 @@ const Resume = ({ data }) => {
             {work.title}
             <span>&bull;</span> <em className="date">{work.years}</em>
           </p>
-          <p>{work.description}</p>
+          {work.company.includes("Wootlab") ? <ul>
+            <li>Part of the team that developed software applications used by an organization (UNDP), targeted at achieving the organization's business objectives. These applications handled over 700,000 records for a national program. - <b>NJFP</b></li>
+            <li>Responsible for working on a range of projects with React & Nextjs as the primary frontend technologies.</li>
+            <li>Designing and developing appealing interactive websites on a daily basis.</li>
+            <li>Collaborating with UI/UX designers and backend developers in delivering user stories into real software solutions.</li>
+            <li>Working with robust REST API(s) for large enterprise applications</li>
+            <li>Worked with Google places and autocomplete API(s) on a taxi Application (Uber Clone) - React Native</li>
+            <li>Integrated Google Search API on a search application (Google Search Clone) - React</li>
+            <li>Built a number of restful API(s) with Express and Typescript</li>
+            <li>Assisted in training 52 Nigerian youths on web development(HTML, CSS, Javascript & Nodejs) and android development -Basecamp (Bootcamp)</li>
+            <li>Trained a number of kids on game development (scratch programming) - KidsCypher</li>
+          </ul>
+          : <p>{work.description}</p>
+          }
         </div>
       );
     });
